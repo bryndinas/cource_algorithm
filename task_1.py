37710fcbc2b4f8f44bab76305e00cbc99f93aa3d
Артем <cooler292@mail.ru>
2020-10-12 22:15:57 +0300

    Задание 1

--- task_1.py
+++ task_1.py
@@ -2,14 +2,21 @@ from uuid import uuid4
 import time
 
 start_val = time.time()
-length = 100000
+length = 10000
 lst = [uuid4().hex for i in range(length)]
 end_val = time.time()
-time = end_val - start_val
-print(start_val)
-print(end_val)
-print(time)
+time_lst = end_val - start_val
 
+print(time_lst)
+
+
+start_val_dict = time.time()
+length_dict = 100000
+dict = {uuid4().hex for el in range(length_dict)}
+end_val_dict = time.time()
+time_dict = end_val_dict - start_val_dict
+
+print(time_dict)
 
 
 
