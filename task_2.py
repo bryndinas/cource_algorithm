046df17e763a3e913d946215092605fb4fd81336
Артем <cooler292@mail.ru>
2020-10-15 20:42:45 +0300

    задание 2

---
+++ task_2.py
@@ -0,0 +1,26 @@
+"""
+Задание 2.
+
+Приведен код, который формирует из введенного числа
+обратное по порядку входящих в него цифр.
+Задача решена через рекурсию
+
+Сделайте замеры времени выполнения кода с помощью модуля timeit
+
+Попробуйте оптимизировать код, чтобы снизить время выполнения
+Проведите повторные замеры
+
+Подсказка: примените мемоизацию
+
+Добавьте аналитику: что вы сделали и почему
+"""
+import timeit
+
+def recursive_reverse(number):
+    if number == 0:
+        return str(number % 10)
+    return f'{str(number % 10)}{recursive_reverse(number // 10)}'
+
+number = 161519161651651655165165165
+
+print(timeit.timeit("recursive_reverse(number)", setup="from __main__ import recursive_reverse, number",number=10000))
